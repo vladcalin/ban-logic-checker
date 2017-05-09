f1d97bca6b4cb37ca149bb628e7263dabb096319
berendeanicolae <berendeanicolae@gmail.com>
2017-05-09 22:39:39 +0300

    bug fix

---
+++ rules.py
@@ -0,0 +1,14 @@
+class Rule(object):
+    pass
+
+class Decryption(Rule):
+    pass
+
+class NonceVerification(Rule):
+    pass
+
+class Jurisdiction(Rule):
+    pass
+
+class Decomposition(Rule):
+    pass
